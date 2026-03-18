facde1ba7d44e8de28bd7cc9a57df698df4ceb49
Zac1017 <9964262@bedfordnhk12.net>
2026-03-18 17:13:27 -0400

    Refactor AutoAdjuster to use specific limelight instance and enhance LED control logic. Actually returns values in Smart Dashboard now so YAY!!!!

--- src/main/java/frc/robot/subsystems/AutoAdjuster.java
+++ src/main/java/frc/robot/subsystems/AutoAdjuster.java
@@ -1,18 +1,26 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.util.LimelightHelpers;
 
 public class AutoAdjuster extends SubsystemBase {
-    private final String limeLightName = "limelight";
+    private final String limeLightName = "limelight-intake";
+    private final NetworkTable limeLightTable;
+    private final NetworkTableInstance tx;
 
     public AutoAdjuster(){
-        
+        limeLightTable = NetworkTableInstance.getDefault().getTable(limeLightName);
+        tx = limeLightTable.getEntry("tx").getInstance();
     }
     @Override
     public void periodic(){
         boolean hasTarget = LimelightHelpers.getTV(limeLightName);
+        LimelightHelpers.setLEDMode_PipelineControl(limeLightName);
+        LimelightHelpers.setLEDMode_ForceOn(limeLightName);
+        //SmartDashboard.putNumber
         if(hasTarget){
             double xOffset = LimelightHelpers.getTX(limeLightName);
             double yOffset = LimelightHelpers.getTY(limeLightName);
@@ -21,6 +29,8 @@ public class AutoAdjuster extends SubsystemBase {
             SmartDashboard.putNumber("X Offset", xOffset);
             SmartDashboard.putNumber("Y Offset", yOffset);
             SmartDashboard.putNumber("Area", area);
+            LimelightHelpers.setLEDMode_ForceOff(limeLightName);
+            
         } else {
             SmartDashboard.putBoolean("Has Target", false);
         }
