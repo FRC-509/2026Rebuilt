d186b3de6fd5c18d469315d15036f9f131f87eef
CouchPugtato <dylanaulson@gmail.com>
2026-03-28 23:35:58 -0400

    End of day 1

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -140,7 +140,7 @@ public class Turret extends SubsystemBase {
     public enum AimTarget {
 
         NONE(Translation3d.kZero, 0, 0),
-        HUB(new Translation3d(4.55,Constants.Field.kFieldWidth/2,1.88), -0.075, 1.8),
+        HUB(new Translation3d(4.35,Constants.Field.kFieldWidth/2,1.88),0, 1.8),
         NEUTRALZONE_FEED_LEFT(new Translation3d(2,Constants.Field.kFieldWidth - 2,0),0, 0),
         NEUTRALZONE_FEED_RIGHT(new Translation3d(2,2,0), 0, 0),
         OPPOSING_ALLIANCE_FEED_LEFT(new Translation3d(2,Constants.Field.kFieldWidth - 2,0),0, 0),
